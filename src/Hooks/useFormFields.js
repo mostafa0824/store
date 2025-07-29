bc5f63eefd1f5195a8c23de5dba0ea5b72b3e570
MOSTAFA <mhrmo75@gmail.com>
2025-07-29 23:26:13 +0330

    remove item

--- src/Hooks/useFormFields.js
+++ src/Hooks/useFormFields.js
@@ -1,11 +1,10 @@
-import { useDebugValue, useState } from "react";
+import { useState } from "react";
 
 const useFormFields = (initialState={}) => {
   const [fields, setFields] = useState(initialState);
   const handleChange = (e) => {
     setFields({ ...fields, [e.target.name]: e.target.value });
   };
-  useDebugValue('custom hook for get input data')
   return [fields, handleChange];
 };
 
