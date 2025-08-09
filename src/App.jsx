712c8fc13df2b327dc0c2fa83670cce277ccf985
MOSTAFA <mhrmo75@gmail.com>
2025-08-09 23:35:40 +0330

    Introduction to the whole project (react redux)

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Product from "./Pages/Product";
 import ProductDetails from "./Pages/ProductDetails";
-import { AuthContext } from "./Context/AuthContext";
+// import { AuthContext } from "./Context/AuthContext";
 import Auth from "./Pages/Auth";
 import NotFound from "./Pages/NotFound";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
+import Cart from "./Pages/Cart";
 
 export default function App() {
-  const { token } = useContext(AuthContext);
+  // const { token } = useContext(AuthContext);
+  const {token}=useSelector(store=>store.auth)
   return (
     <>
     <Navbar/>
@@ -21,8 +24,8 @@ export default function App() {
         <Route path="/product-details/:id/:name" element={<ProductDetails/>} />
         <Route
           path="/auth"
-          element={token ? <Navigate to={"/"} /> : <Auth />}
-        />
+          element={token ? <Navigate to={"/"} /> : <Auth />}/>
+        <Route path="/cart" element={token?<Cart/>:<Auth/>} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
       <Footer/>
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AuthContextProvider from './Context/AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from "./Store";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-   <AuthContextProvider>
-     <App /> 
-   </AuthContextProvider>
+   <Provider store={store}>
+     <App />
+     </Provider>
    </BrowserRouter>
   </StrictMode>,
 )
