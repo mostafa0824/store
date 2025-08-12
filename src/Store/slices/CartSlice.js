94599241b978a8abeee920209d825dec7fe386fe
MOSTAFA <mhrmo75@gmail.com>
2025-08-12 23:15:19 +0330

    reforms

--- src/Store/slices/CartSlice.js
+++ src/Store/slices/CartSlice.js
@@ -4,9 +4,11 @@ const savedCart = JSON.parse(localStorage.getItem("cart")) || [];
 
 const initialState = {
   items: savedCart,
-  totalPrice: savedCart.reduce((sum, item) => sum + item.price * item.cartQuantity, 0)
+  totalPrice: savedCart.reduce(
+    (total, sum) => total + sum.price * sum.cartQuantity,
+    0
+  ),
 };
-
 const cartSlice = createSlice({
   name: "cart",
   initialState,
@@ -15,29 +17,24 @@ const cartSlice = createSlice({
       const item = action.payload;
       const find = state.items.find((pr) => pr.id === item.id);
 
-      if (find) {
-        find.cartQuantity++;
-      } else {
-        state.items.push({ ...item, cartQuantity: 1 });
-      }
+      find
+        ? find.cartQuantity++
+        : state.items.push({ ...item, cartQuantity: 1 });
 
       state.totalPrice += item.price;
       localStorage.setItem("cart", JSON.stringify(state.items));
     },
-
     removeItem: (state, action) => {
       const id = action.payload;
-      const item = state.items.find((pr) => pr.id === id);
+      const find = state.items.find((pr) => pr.id === id);
 
-      if (item) {
-        state.totalPrice -= item.price;
-        item.cartQuantity--;
-
-        if (item.cartQuantity === 0) {
-          state.items = state.items.filter((pr) => pr.id !== id);
-        }
+      if (find) {
+        state.totalPrice -= find.price;
+        find.cartQuantity--;
       }
-
+      if (find.cartQuantity === 0) {
+       state.items= state.items.filter((pr) => pr.id !== id);
+      }
       localStorage.setItem("cart", JSON.stringify(state.items));
     },
 
@@ -45,8 +42,8 @@ const cartSlice = createSlice({
       state.items = [];
       state.totalPrice = 0;
       localStorage.removeItem("cart");
-    }
-  }
+    },
+  },
 });
 
 export const { addItem, removeItem, clearCart } = cartSlice.actions;
